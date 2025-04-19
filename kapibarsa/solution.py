5fb25a11403617fe1439257886f60c6e9efe86ba
Dfmari <damir.riyatov@gmail.com>
2025-04-19 20:22:38 +0500

    solved kapibarsa

--- kapibarsa/solution.py
+++ kapibarsa/solution.py
@@ -5,4 +5,19 @@
 3 - Y
 '''
 num = 414363270630243
-
+flag = ''
+while num != 1:
+    extra = num % 4
+    if extra == 0:
+        flag = 'G' + flag
+        num = num // 4
+    if extra == 1:
+        flag = 'B' + flag
+        num = (num - 1) // 4
+    if extra == 2:
+        flag = 'R' + flag
+        num = (num - 2) // 4
+    if extra == 3:
+        flag = 'Y' + flag
+        num = (num - 3) // 4
+print('tctf{' + flag + '}')
